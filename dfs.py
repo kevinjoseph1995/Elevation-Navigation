d611b0adadb9869a408e7275b334f37ffb789262
shreesh <shreesh.ladha@gmail.com>
2018-11-17 06:23:05 -0800

    Added dijkstra implementation and changed graph init method

--- dfs.py
+++ dfs.py
@@ -1,14 +1,25 @@
+from collections import deque, defaultdict
+from heapq import *
 import networkx as nx 
 import osmnx as ox 
 import requests  
-G = ox.graph_from_place('Piedmont, California, USA', network_type='drive')
-G_proj = ox.project_graph(G)
-origin = ox.get_nearest_node(G, (37.824044, -122.233651))
-dest = ox.get_nearest_node(G, (37.826059, -122.240346))
+# G = ox.graph_from_place('Piedmont, California, USA', network_type='walk')
+org, dst = (37.826003, -122.23309), (37.817440, -122.250660)
+G = ox.graph_from_point(org, distance=2000)
+bbox = ox.bbox_from_point(org, distance=2000)
 
-r = nx.shortest_path(G_proj, source=origin, target=dest, weight='length')
-shortest = sum(ox.get_route_edge_attributes(G_proj, r, 'length'))
+
 G = ox.add_node_elevations(G, api_key=google_elevation_api_key)
+G = ox.add_edge_grades(G)
+
+G_proj = ox.project_graph(G)
+origin = ox.get_nearest_node(G, org, return_dist = True)
+dest = ox.get_nearest_node(G, dst, return_dist = True)
+if origin[1] > 100 or dest[1] > 100:
+    raise ValueError("Nodes found are inaccurate!!")
+
+r = nx.shortest_path(G_proj, source=origin[0], target=dest[0], weight='length')
+shortest = sum(ox.get_route_edge_attributes(G_proj, r, 'length'))
 
 def getCost(n1, n2, mode = "normal"):
     if mode == "normal":
@@ -42,9 +53,48 @@ def dfs(src, currDist, currElevDist, path, target, best):
     visited.remove(src)
     return
 
+def dijkstra(src, target):
+    q, seen, mins = [(0,src,[])], set(), {src: 0}
+    while q:
+        cost, node, path = heappop(q)
+        if node not in seen:
+            seen.add(node)
+            path.append(node)
+            if node == target: return cost
+
+            for nei in G.neighbors(node):
+                if nei in seen: continue
+                prev = mins.get(nei, None)
+                next = cost + getCost(node, nei)
+                if prev is None or next < prev:
+                    mins[nei] = next
+                    heappush(q, (next, nei, path))
+
+    return float("inf")
+
+#need to see which one is faster
+def dijkstra_2(src, target):
+    dist = {}
+    queue = [(0, src)]
+    while queue:
+        cost, node = heappop(queue)
+        if node == target:
+            return dist
+        if node not in dist: # v is unvisited
+            dist[node] = cost
+            for nei in G.neighbors(node):
+                if nei not in dist:
+                    heappush(queue, (cost + getCost(node, nei), nei))
+
+    # to give same result as original, assign zero distance to unreachable vertices             
+    return float('inf')
+    # return [0 if x is None else x for x in A]
+
 normal = 0
 visited = set()
-x = 50.0/100
+x = 10.0/100
 best = [[float("-inf"), float("inf")], []]
-dfs(origin, 0.0, 0.0, [origin], dest, best)
+dfs(origin[0], 0.0, 0.0, [origin[0]], dest[0], best)
+bfs(origin[0], dest[0], best)
+dijkstra(origin[0], dest[0])
 print(best)
